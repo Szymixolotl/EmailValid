39e854063ee8278d87e805a2a486f7ddeb283436
Szymixolotl <117274573+Szymixolotl@users.noreply.github.com>
2024-05-17 13:30:59 +0200

    Zrobiony skrypt aplikacja zaczęła działać
    Dopisano skrypt do walidacji

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-
+function czytoemail(){
+    inpucik = document.getElementById("email");
+    inputValue = inpucik.value
+    var re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
     
-
+    if(re.test(inputValue) == true){
+        element = document.getElementById("poprawnosc");
+        element.classList.remove("zle");
+        element.classList.add("git");
+    }
+    else{
+        element = document.getElementById("poprawnosc");
+        element.classList.remove("git");
+        element.classList.add("zle");
+    }
+    console.log(re.test(inputValue)) ;
+}
